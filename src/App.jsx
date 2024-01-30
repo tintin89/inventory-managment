3ed9750e23f22d407d4d88744c027c8a6bfc8abd
Nivaldo <nivalditin89@gmail.com>
2024-01-30 02:46:56 -0800

    sidebar_added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { AuthContextProvider } from "./context/authContext";
 import { MyRoutes } from "./routers/routes";
 import { createContext, useState } from "react";
 import { Dark, Light } from "./styles/themes";
+import { Device } from "./styles/breackpoints";
+import { Sidebar } from "./components/organism/sidebar/Sidebar";
 
 export const ThemeContext = createContext(null);
 
@@ -10,12 +12,15 @@ function App() {
   const [themeuse, setTheme] = useState("dark");
   const theme = themeuse === "light" ? "light" : "dark";
   const themeStyle = theme === "light" ? Light : Dark;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <ThemeProvider theme={themeStyle}>
         <AuthContextProvider>
-          <Container>
-            <section className="ContentSidebar">Sidebar</section>
+          <Container className={sidebarOpen ? "active" : ""}>
+            <section className="ContentSidebar">
+              <Sidebar state={sidebarOpen} setState={()=>setSidebarOpen(!sidebarOpen)}/>
+            </section>
             <section className="ContentMenuHam"> Burger Menu</section>
             <section className="ContentRoutes">
               <MyRoutes />
@@ -27,10 +32,37 @@ function App() {
   );
 }
 
-const Container = styled.div`
+const Container = styled.main`
   display: grid;
   grid-template-columns: 1fr;
   background-color: ${({ theme }) => theme.bgtotal};
+  .ContentSidebar {
+    display: none;
+  }
+  .ContentmenuHam {
+    display: block;
+    position: absolute;
+    left: 20px;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 65px 1fr;
+    &.active {
+      grid-template-columns: 220px 1fr;
+    }
+    .ContentSidebar {
+      display: initial;
+    }
+    .ContentMenuHam {
+      display: none;
+    }
+  }
+  .ContentRoutes {
+    grid-column: 1;
+    width: 100%;
+    @media ${Device.tablet} {
+      grid-column: 2;
+    }
+  }
 `;
 
 export default App;
--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -4,13 +4,20 @@ import styled from 'styled-components'
 function HomeTemplate() {
   return (
     <Container>
-        <h1>Home</h1>
+        <h1>Home Template</h1>
     </Container>
   )
 }
 
 const Container = styled.div`
-  /* ... */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+  background-color: ${({theme})=>theme.bgtotal};
+  color: ${({theme})=>theme.text};
+  width: 100%;
 `;
 
 export default HomeTemplate
