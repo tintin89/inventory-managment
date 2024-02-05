55bab585c02c238656e6c50f8f036204555851fb
Nivaldo <nivalditin89@gmail.com>
2024-02-05 08:12:27 -0800

    added store to register users

--- src/store/UserStore.jsx
+++ src/store/UserStore.jsx
@@ -1,4 +1,9 @@
 import { create } from "zustand";
-export const useUserStore = create((get,set)=>({
-    
-}))
+export const useUserStore = create((get, set) => ({
+  adminRegister: async (p) => {
+    await supabase.auth.signUp({
+      email: p.email,
+      password: p.password,
+    });
+  },
+}));
