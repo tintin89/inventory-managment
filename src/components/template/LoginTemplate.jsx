64e6a31fef1f992d9c75d6d1686af1b248e56416
Nivaldo <nivalditin89@gmail.com>
2024-02-12 01:45:53 -0800

    tanstack query added

--- src/components/template/LoginTemplate.jsx
+++ src/components/template/LoginTemplate.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import {Btnsave} from '../molecules/BtnSave';
 import {useUserStore} from '../../store/UserStore';
+import { useMutation } from "@tanstack/react-query";
 
 function LoginTemplate() {
   const {adminRegister} = useUserStore();
+  const mutation = useMutation({
+    mutationKey:['Register admin user'],
+    mutationFn: async ()=>await adminRegister()
+  })
   return <Container>
     <Btnsave title="Signup" bgcolor="#fff"/>
   </Container>;
